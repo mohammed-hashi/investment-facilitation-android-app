7a93979b2b4572ade94515d501814c89b34f8c3c
Mohammed <mohamedmeecaad@gmail.com>
2026-02-07 12:20:25 +0300

    Add LanguageSwitch widget for language selection

--- lib/widgets/language_switch.dart
+++ lib/widgets/language_switch.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../core/language_provider.dart';
+
+class LanguageSwitch extends StatelessWidget {
+  const LanguageSwitch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      icon: const Icon(Icons.language),
+      onSelected: (value) {
+        Provider.of<LanguageProvider>(context, listen: false)
+            .changeLanguage(value);
+      },
+      itemBuilder: (context) => [
+        const PopupMenuItem(value: 'en', child: Text('English')),
+        const PopupMenuItem(value: 'so', child: Text('Somali')),
+      ],
+    );
+  }
+}
 
